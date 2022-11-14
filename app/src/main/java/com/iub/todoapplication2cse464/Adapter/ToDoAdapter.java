d1265e834c6507aa4060a96f12f9a825ce37df9e
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2022-11-14 18:32:55 +0600

    splash screen

---
+++ app/src/main/java/com/iub/todoapplication2cse464/Adapter/ToDoAdapter.java
@@ -0,0 +1,52 @@
+package com.iub.todoapplication2cse464.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.iub.todoapplication2cse464.MainActivity;
+import com.iub.todoapplication2cse464.Model.ToDoModel;
+import com.iub.todoapplication2cse464.R;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
+    private List<ToDoModel> todoList;
+    private MainActivity activity;
+
+    public ToDoAdapter(MainActivity activity){
+        this.activity = activity;
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_layout,parent,false);
+        return new ViewHolder(itemView);
+    }
+
+    public void onBindViewHolder(ViewHolder holder, int position){
+        ToDoModel item = todoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+    }
+
+    public int getItemCount(){
+        return todoList.size();
+    }
+
+    private boolean toBoolean(int n){
+        return n!=0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        CheckBox task;
+
+        ViewHolder(View view){
+            super(view);
+            task = view.findViewById(R.id.todoCheckBox);
+        }
+    }
+}
