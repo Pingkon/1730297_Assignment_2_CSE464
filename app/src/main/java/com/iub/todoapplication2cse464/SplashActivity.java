294cd99c7ac4b7bd193158a038f5cece279254cf
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2022-11-14 18:04:52 +0600

    layout update

---
+++ app/src/main/java/com/iub/todoapplication2cse464/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.iub.todoapplication2cse464;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
