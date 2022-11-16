10394a99556aa58ce43ba3a09130e45f91ef12ef
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2022-11-16 18:19:54 +0600

    database Added

--- app/src/main/java/com/iub/todoapplication2cse464/MainActivity.java
+++ app/src/main/java/com/iub/todoapplication2cse464/MainActivity.java
@@ -5,10 +5,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+
+import com.iub.todoapplication2cse464.Adapter.ToDoAdapter;
+import com.iub.todoapplication2cse464.Model.ToDoModel;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+
+    private List<ToDoModel> taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +27,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        taskList = new ArrayList<>();
+
         tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
         tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("This is a Test Task");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+
+        tasksAdapter.setTask(taskList);
     }
 }
